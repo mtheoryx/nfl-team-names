e43829e844cc24a2f9a3cceab91bd4b4f88e1d1b
David R. Poindexter III <drpoindexter@gmail.com>
2015-10-10 15:29:03 -0400

    feat(random): Add ability to get an array of nfl team names
    If you pass a number to the random method, you will receive an array
    with that number of random nfl
    team names
    
    closes #2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 var uniqueRandomArray = require('unique-random-array');
 var nflNames = require('./nfl-team-names.json');
+var getRandomName = uniqueRandomArray(nflNames);
 
 module.exports = {
 	all: nflNames,
-	random: uniqueRandomArray(nflNames)
+	random: random
 };
+
+function random(number) {
+	if( number === undefined ) {
+		return getRandomName();
+	} else {
+		var randomNames = [];
+		for (var i = 0; i < number; i++) {
+			randomNames.push(getRandomName());
+		}
+		return randomNames;
+	}
+}
--- src/index.test.js
+++ src/index.test.js
@@ -14,15 +14,25 @@ describe('nfl-names', function() {
 		});
 
 		it('should always include the Indianpolis Colts', function() {
-			"use strict";
 			expect(nflNames.all).to.include('Colts');
 		});
 	});
 
 	describe('random', function() {
-		it('should return a random item from the nflNames.random', function() {
-			var randomItem = nflNames.random();
-			expect(nflNames.all).to.include(randomItem);
+		it('should return a random nfl team name from the nflNames.random', function() {
+			var randomName = nflNames.random();
+
+			expect(nflNames.all).to.include(randomName);
+		});
+
+		it('should return an array of random names if passed a number', function() {
+			var randomNames = nflNames.random(3);
+
+			expect(randomNames).to.have.length(3);
+
+			randomNames.forEach(function (name) {
+				expect(nflNames.all).to.include(name);
+			});
 		});
 	});
 });
